96c90d78e4c23eb6b542a42ee53da3a8e7c0e851
SergeevA <workhorns@gmail.com>
2023-03-29 08:18:58 +0700

    Перемещение Js фалов

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import {createStore} from 'redux'
 import {Provider}  from 'react-redux';
-import reducer from './reduser';
+import reducer from './redusers/reduser';
 import App from './components/app';
 
 const store = createStore(reducer);
